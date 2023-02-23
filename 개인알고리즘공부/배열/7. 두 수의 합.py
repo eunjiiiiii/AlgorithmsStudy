aa66b570cf27a1f9ee403c1dbc1489f224683555
eunjiiiiii <47842737+eunjiiiiii@users.noreply.github.com>
2023-02-23 13:37:36 +0900

    [파알인] 배열 7. 두 수의 합

--- 개인알고리즘공부/배열/7. 두 수의 합.py
+++ 개인알고리즘공부/배열/7. 두 수의 합.py
@@ -14,9 +14,33 @@ def twoSum_in(nums, target):
 
             return [nums.index(num), nums[i+1:].index(rest) + (i+1)]
 
+def twoSum_dict(nums, target):
+    """
+    in 연산자가 아닌 딕셔너리 이용해서 한번에 조회
+    :param nums:
+    :param target:
+    :return:
+    """
+
+    num_dict = {}
+
+    for i, num in enumerate(nums):
+        num_dict[num] = i
+
+    for i , num in enumerate(nums):
+        if target - num in num_dict and num_dict[target - num] != i:
+            return [i, num_dict[target- num]]
+
 
 if __name__ == "__main__":
     nums = [2, 7, 11, 15]
     target = 9
 
-    print(twoSum_in(nums, target))
+    #print(twoSum_in(nums, target))
+    print(twoSum_dict(nums, target))
+
+    '''
+    ex_dict = {'one':1, 'two': 2}
+    if 1 in ex_dict:
+        print(True)
+    '''
