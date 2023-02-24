c11f09709f26a03daaf4762afec8fae45bd83225
eunjiiiiii <ej27470235@gmail.com>
2023-02-24 13:18:26 +0900

    [백준] 2206. 벽 부수고 이동하기_bfs

--- 개인알고리즘공부/그래프/2206. 벽 부수고 이동하기.py
+++ 개인알고리즘공부/그래프/2206. 벽 부수고 이동하기.py
@@ -1,45 +1,51 @@
-def dfs(i, j, map):
-    path = 0  # 경로 저장하는 변수
-    if i < 0 or i >= len(map) or \
-        j < 0 or j >= len(map[0]) or \
-        map[i][j] == 1:
-            return  # 종료
+from collections import deque
 
-
-    if map[i][j] == 0:
-        # 이미 조회한 노드는 1로 바꿔주기
-        map[i][j] = 1
-
-        # 동서남북 조회
-        dfs(i - 1, j)
-        dfs(i + 1, j)
-        dfs(i, j - 1)
-        dfs(i, j + 1)
-
-        path += 1
-
-    return path
-
-# map 크기 입력 받기
+# 1. 입력 받기
 n, m = map(int, input().split())
-# for문으로 map 입력 받기
-map = []
+
+graph = []
 for i in range(n):
-    # 한 행의 원소들을 리스트로 한꺼번에 받기
-    map.append(list(map(int,input())))  # map에 리스트를 한 행씩 저장하여 2차원 리스트 만들기
+    graph.append(list(map(int, input()))) # nxm 2차원 배열로 받기
+    
+# 2. 상하좌우 방향 설정하기
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
+# 3. 벽을 깼는지 여부를 나타내는 3차원 배열 만들기
+#    벽을 깼을 때(c=1)의 경로와 벽을 깨지 않았을 때(c=0)의 경로로 나눠서 비교하려고
+visited = [[[0]*2 for _ in range(m)] for _ in range(n)]
+visited[0][0][0] = 1  # graph[0,0]은 조회했다고 1 넣어 주고 시작
 
-one_count = 0  # 1 깨부신 횟수
-path_list = []  # 경로 경우 저장하는 리스트
-# dfs 이용한 최단경로 구하는 프로그램 구현
-for i in range(n):
-    for j in range(m):
-        # 1이면
-        if map[i][j] == 1:
-            path_list.append(dfs(i, j, map))
-            if one_count > 1:  # 최대 1번만 깨부시는게 가능하므로
-                print(-1)
-            one_count += 1
+def bfs(x, y, z):  # 시작 좌표를 매개변수로 받아 옴
+    queue = deque()
+    queue.append((x,y,z)) # queue에 저장할 값은 튜플로 관리!(좌표이므로)
 
-print(min(path_list))
+    while queue: # queue가 비기 전까지
+        a,b,c = queue.popleft()  # 1. queue pop 사용!!!!!
 
+        # 2. 끝 좌표인지 check
+        if a == n-1 and b == m-1:
+            return visited[a][b][c] # 정상적인 처리 시 빠져나가는 루트
+
+        # 3. 상하좌우 이동 : nx, ny 설정
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+
+            # 4. nx, ny 좌표 유효 여부 check
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            # 5. 진짜 처리
+            # 1) 다음에 이동할 곳이 벽이고, 아직 한번도 깨부신적이 없다면
+            if graph[nx][ny] == 1 and c == 0:
+                visited[nx][ny][1] = visited[a][b][0] + 1 # c=1로 새로 이동한 칸에 칸수 저장
+                queue.append((nx, ny, 1)) # c = 1
+            # 2) 다음에 이동할 곳이 벽이 아니고, 아직 한번도 방문하지 않은 곳이면
+            elif graph[nx][ny] == 0 and visited[nx][ny][c] == 0:
+                visited[nx][ny][c] = visited[a][b][c] + 1
+                queue.append((nx, ny, c))
+
+    return -1 # 비정상적인 처리 시 빠져나가는 루트
+
+print(bfs(0,0,0))
